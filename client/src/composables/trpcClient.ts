234d7c230c8e2f6fb3e26ffbc86fb75cb630e67e
Kyle <lauber.kyle@gmail.com>
2022-07-18 23:11:32 -0700

    chore: vue-trpc-query

--- client/src/composables/trpcClient.ts
+++ client/src/composables/trpcClient.ts
@@ -3,6 +3,7 @@ import { httpBatchLink } from '@trpc/client/links/httpBatchLink'
 import { loggerLink } from '@trpc/client/links/loggerLink'
 import superjson from 'superjson'
 import type { AppRouter } from '../../../server/src/trpc/route/app.router'
+import { createVueQueryHooks } from './createVueQueryHooks'
 
 const url = 'http://localhost:2022/trpc'
 
@@ -16,3 +17,6 @@ export const client = createTRPCClient<AppRouter>({
   ],
   transformer: superjson,
 })
+
+export const trpc = createVueQueryHooks<AppRouter>()
+
